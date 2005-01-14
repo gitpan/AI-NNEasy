02917341c1993ce0ef8939e58474904b671ab9b2
Graciliano Monteiro Passos <gmpassos@cpan.org>
2005-01-14 04:01:49 +0000

    Import of GMPASSOS/AI-NNEasy-0.01 from CPAN.
    gitpan-cpan-distribution: AI-NNEasy
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         GMPASSOS/AI-NNEasy-0.01.tar.gz
    gitpan-cpan-author:       GMPASSOS
    gitpan-cpan-maturity:     released

---
+++ lib/AI/NNEasy.pm
@@ -0,0 +1,676 @@
+#############################################################################
+## This file was generated automatically by Class::HPLOO/0.18
+##
+## Original file:    ./lib/AI/NNEasy.hploo
+## Generation date:  2005-01-14 01:54:15
+##
+## ** Do not change this file, use the original HPLOO source! **
+#############################################################################
+
+#############################################################################
+## Name:        NNEasy.pm
+## Purpose:     AI::NNEasy
+## Author:      Graciliano M. P. 
+## Modified by:
+## Created:     2005-01-14
+## RCS-ID:      
+## Copyright:   (c) 2005 Graciliano M. P. 
+## Licence:     This program is free software; you can redistribute it and/or
+##              modify it under the same terms as Perl itself
+#############################################################################
+
+
+{ package AI::NNEasy ;
+
+  use strict qw(vars) ; no warnings ;
+
+  use vars qw(%CLASS_HPLOO @ISA $VERSION) ;
+
+  $VERSION = '0.01' ;
+
+  @ISA = qw(Class::HPLOO::Base UNIVERSAL) ;
+
+  my $CLASS = 'AI::NNEasy' ; sub __CLASS__ { 'AI::NNEasy' } ;
+
+  use Class::HPLOO::Base ;
+
+  use AI::NNEasy::NN ;
+  use Storable qw(freeze thaw) ;
+  use Data::Dumper ;
+
+  sub NNEasy { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $file = shift(@_) ;
+    my @out_types = ref($_[0]) eq 'ARRAY' ? @{ shift(@_) } : ( ref($_[0]) eq 'HASH' ? %{ shift(@_) } : shift(@_) ) ;
+    my $error_ok = shift(@_) ;
+    my $in = shift(@_) ;
+    my $out = shift(@_) ;
+    my @layers = ref($_[0]) eq 'ARRAY' ? @{ shift(@_) } : ( ref($_[0]) eq 'HASH' ? %{ shift(@_) } : shift(@_) ) ;
+    my $conf = shift(@_) ;
+    
+    $file ||= 'nneasy.nne' ;
+  
+    if ( $this->load($file) ) {
+      return $this ;
+    }
+  
+    my $in_sz  = ref $in  ? $in->{nodes}  : $in ;
+    my $out_sz = ref $out ? $out->{nodes} : $out ;
+  
+    @layers = ($in_sz+$out_sz) if !@layers ;
+    
+    foreach my $layers_i ( @layers ) {
+      $layers_i = $in_sz+$out_sz if $layers_i <= 0 ;
+    }
+    
+    $in  = {nodes=>$in  , persistent_activation=>0 , decay=>0.0 , random_activation=>0 , threshold=>0.0 , activation_function=>'tanh' , random_weights=>1} if !ref($in) ;
+    $out = {nodes=>$out , persistent_activation=>0 , decay=>0.0 , random_activation=>0 , threshold=>0.0 , activation_function=>'tanh' , random_weights=>1} if !ref($out) ;
+    
+    foreach my $layers_i ( @layers ) {
+      $layers_i = {nodes=>$layers_i, persistent_activation=>0 , decay=>0.0 , random_activation=>0 , threshold=>0.0 , activation_function=>'tanh' , random_weights=>1} if !ref($layers_i) ;
+    }
+    
+    my $nn_conf = {random_connections=>0 , networktype=>'feedforward' , random_weights=>1 , learning_algorithm=>'backprop' , learning_rate=>0.1 , bias=>1} ;
+    
+    foreach my $Key ( keys %$conf ) { $$nn_conf{$Key} = $$conf{$Key} ;}
+    
+    $this->{NN_ARGS} = [[ $in , @layers , $out ] , $nn_conf] ;
+
+    $this->{NN} = AI::NNEasy::NN->new( @{$this->{NN_ARGS}} ) ;
+    
+    $this->{FILE} = $file ;
+    
+    @out_types = (0,1) if !@out_types ;
+    
+    $this->{OUT_TYPES} = [ sort {$a <=> $b} @out_types ] ;
+    
+    $this->{ERROR_OK} = $error_ok || 0.1 ;
+    
+    return $this ;
+  }
+  
+  sub reset_nn { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    
+    $this->{NN} = AI::NNEasy::NN->new( @{ $this->{NN_ARGS} } ) ;
+  }
+  
+  sub load { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $file = shift(@_) ;
+    
+    $file ||= $this->{FILE} ;
+    if ( -s $file ) {
+      open (my $fh, $file) ;
+      my $dump = join '' , <$fh> ;
+      close ($fh) ;
+      
+      my $restored = thaw($dump) ;
+      
+      if ($restored) {
+        my $fl = $this->{FILE} ;
+        %$this = %$restored ;
+        $this->{FILE} = $fl if $fl ;
+        return 1 ;
+      }
+    }
+    return ;
+  }
+  
+  sub save { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $file = shift(@_) ;
+    
+    $file ||= $this->{FILE} ;
+        
+    my $dump = freeze( {%$this} ) ;
+    open (my $fh,">$this->{FILE}") ;
+    print $fh $dump ;
+    close ($fh) ;
+  }
+  
+  sub learn { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $in = shift(@_) ;
+    my $out = shift(@_) ;
+    my $n = shift(@_) ;
+    
+    $n ||= 100 ;
+    
+    my $err ;
+    for (1..100) {
+      $this->{NN}->run($in) ;
+      $err = $this->{NN}->learn($out) ;
+    }
+    
+    $err *= -1 if $err < 0 ;
+    return $err ;
+  }
+  
+  sub learn_set { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my @set = ref($_[0]) eq 'ARRAY' ? @{ shift(@_) } : ( ref($_[0]) eq 'HASH' ? %{ shift(@_) } : shift(@_) ) ;
+    my $ins_ok = shift(@_) ;
+    my $limit = shift(@_) ;
+    my $verbose = shift(@_) ;
+    
+    my $ins_sz = @set / 2 ;
+
+    $ins_ok ||= $ins_sz ;
+    $limit ||= 30000 ;
+  
+    my $error_ok = $this->{ERROR_OK} ;
+    
+    my ($learn_ok,$counter,$err,$err_last,$err_count,$err_static, $reset_count1 , $reset_count2 ,$print) ;
+    
+    $err_static = 0 ;
+    
+    while ( ($learn_ok < $ins_ok) && ($counter < $limit) ) {
+      $learn_ok = 0 ;
+      $err = 0 ;
+      
+      $print = '' if $verbose ;
+      for (my $i = 0 ; $i < @set ; $i+=2) {       
+        $this->{NN}->run($set[$i]) ;
+        my $er = $this->{NN}->learn($set[$i+1]) ;
+        $er *= -1 if $er < 0 ;
+        
+        ++$learn_ok if $er < $error_ok ;
+        $err += $er ;
+        
+        $print .= join(' ',@{$set[$i]}) ." => ". join(' ',@{$set[$i+1]}) ." > $er\n" ;
+      }
+      
+      $err /= $ins_ok ;
+      
+      ++$counter ;
+      
+      if ( !($counter % 100) || $learn_ok == $ins_ok ) {
+        my $err_diff = $err_last - $err ;
+        $err_diff *= -1 if $err_diff < 0 ;
+        
+        $err_count += $err_diff ;
+        
+        ++$err_static if $err_diff <= 0.00001 ;
+        
+        print "err_static = $err_static\n" if $verbose && $err_static ;
+
+        $err_last = $err ;
+        
+        my $inited ;
+        if ( $err_static >= 15 ) {
+          $err_static = 0 ;
+          $counter -= 2000 ;
+          $inited = 1 ;
+          ++$reset_count1 ;
+          
+          if ( ( $reset_count1 + $reset_count2 ) > 2 ) {
+            $reset_count1 = $reset_count2 = 0 ;
+            print "** Reseting NN...\n" if $verbose ;
+            $this->reset_nn ;
+          }
+          else {
+            print "** Reseting weights due NULL diff...\n" if $verbose ;
+            $this->{NN}->init ;
+          }
+        }
+        
+        if ( !($counter % 1000) ) {
+          $err_count /= 10 ;
+          
+          print "ERR COUNT> $err_count\n" if $verbose ;
+          
+          if ( !$inited && $err_count < 0.001 ) {
+            $err_static = 0 ;
+            $counter -= 1000 ;
+            ++$reset_count2 ;
+            
+            if ( ($reset_count1 + $reset_count2) > 2 ) {
+              $reset_count1 = $reset_count2 = 0 ;
+              print "** Reseting NN...\n" if $verbose ;
+              $this->reset_nn ;
+            }
+            else {
+              print "** Reseting weights due LOW diff...\n" if $verbose ;
+              $this->{NN}->init ;
+            }
+          }
+
+          $err_count = 0 ;
+        }
+        
+        if ( $verbose ) {
+          print "\nepoch $counter : error = $err : err_diff = $err_diff : err_static = $err_static : ok = $learn_ok\n" ;
+          print $print ;
+        }
+      }
+
+      print "epoch $counter : error = $err : ok = $learn_ok\n" if $verbose > 1 ;
+      
+    }
+    
+  }
+  
+  sub get_set_error { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my @set = ref($_[0]) eq 'ARRAY' ? @{ shift(@_) } : ( ref($_[0]) eq 'HASH' ? %{ shift(@_) } : shift(@_) ) ;
+    my $ins_ok = shift(@_) ;
+    
+    my $ins_sz = @set / 2 ;
+
+    $ins_ok ||= $ins_sz ;
+  
+    my $err ;
+    for (my $i = 0 ; $i < @set ; $i+=2) {
+      $this->{NN}->run($set[$i]) ;
+      my $er = $this->{NN}->RMSErr($set[$i+1]) ;
+      $er *= -1 if $er < 0 ;
+      $err += $er ;
+    }
+    
+    $err /= $ins_ok ;
+    return $err ;
+  }
+  
+  sub run { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $in = shift(@_) ;
+    
+    $this->{NN}->run($in) ;
+    my $out = $this->{NN}->output() ;
+    return $out ;
+  }
+  
+  sub run_get_winner { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    
+    my $out = $this->run(@_) ;
+    
+    foreach my $out_i ( @$out ) {
+      $out_i = $this->out_type_winner($out_i) ;
+    }
+    
+    return $out ;
+  }
+  
+  sub out_type_winner { 
+    my $this = ref($_[0]) ? shift : undef ;
+    my $CLASS = ref($this) || __PACKAGE__ ;
+    my $val = shift(@_) ;
+    
+    my ($out_type , %err) ;
+    
+    foreach my $types_i ( @{ $this->{OUT_TYPES} } ) {
+      my $er = $types_i - $val ;
+      $er *= -1 if $er < 0 ;
+      
+      if ( $er < $this->{ERROR_OK} ) {
+        $out_type = $types_i ;
+        last ;
+      }
+      
+      $err{$types_i} = $er ;
+    }
+    
+    if ( !defined $out_type ) {
+      my $min_type_err = (sort { $err{$a} <=> $err{$b} } keys %err)[0] ;
+      $out_type = $min_type_err ;
+    }
+
+    return $out_type ;
+  }
+
+
+
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+AI::NNEasy - Define, learn and use easy Neural Networks of different types using a portable code in Perl and XS.
+
+=head1 DESCRIPTION
+
+The main purpose of this module is to create easy Neural Networks with Perl.
+
+The module was designed to can be extended to multiple network types, learning algorithms and activation functions.
+This architecture as 1st based in the module L<AI::NNFlex>, than I have rewrited it to fix some
+serialization bugs, and have otimized the code and added some XS functions to get speed
+in the learning process. Finally I have added an intuitive inteface to create and use the NN,
+and added a winner algorithm to the output.
+
+I have writed this module because after test different NN module on Perl I can't find
+one that is portable through Linux and Windows, easy to use and the most important,
+one that really works in a reall problem.
+
+With this module you don't need to learn much about NN to be able to construct one, you just
+define the construction of the NN, learn your set of inputs, and use it.
+
+=head1 USAGE
+
+Here's an example of a NN to compute XOR:
+
+  use AI::NNEasy ;
+  
+  ## Our maximal error for the output calculation.
+  my $ERR_OK = 0.1 ;
+
+  ## Create the NN:
+  my $nn = AI::NNEasy->new(
+  'xor.nne' , ## file to save the NN.
+  [0,1] ,     ## Output types of the NN.
+  $ERR_OK ,   ## Maximal error for output.
+  2 ,         ## Number of inputs.
+  1 ,         ## Number of outputs.
+  [3] ,       ## Hidden layers. (this is setting 1 hidden layer with 3 nodes).
+  ) ;
+  
+  
+  ## Our set of inputs and outputs to learn:
+  my @set = (
+  [0,0] => [0],
+  [0,1] => [1],
+  [1,0] => [1],
+  [1,1] => [0],
+  );
+  
+  ## Calculate the actual error for the set:
+  my $set_err = $nn->get_set_error(\@set) ;
+  
+  ## If set error is bigger than maximal error lest's learn this set:
+  if ( $set_err > $ERR_OK ) {
+    $nn->learn_set( \@set ) ;
+    ## Save the NN:
+    $nn->save ;
+  }
+  
+  ## Use the NN:
+  
+  my $out = $nn->run_get_winner([0,0]) ;
+  print "0 0 => @$out\n" ; ## 0 0 => 0
+  
+  my $out = $nn->run_get_winner([0,1]) ;
+  print "0 1 => @$out\n" ; ## 0 1 => 1
+  
+  my $out = $nn->run_get_winner([1,0]) ;
+  print "1 0 => @$out\n" ; ## 1 0 => 1
+  
+  my $out = $nn->run_get_winner([1,1]) ;
+  print "1 1 => @$out\n" ; ## 1 1 => 0
+  
+  ## or just interate through the @set:
+  for (my $i = 0 ; $i < @set ; $i+=2) {
+    my $out = $nn->run_get_winner($set[$i]) ;
+    print "@{$set[$i]}) => @$out\n" ;
+  }
+
+=head1 METHODS
+
+=head2 new ( FILE , @OUTPUT_TYPES , ERROR_OK , IN_SIZE , OUT_SIZE , @HIDDEN_LAYERS , %CONF )
+
+=over 4
+
+=item FILE
+
+The file path to save the NN. Default: 'nneasy.nne'.
+
+=item @OUTPUT_TYPES
+
+An array of outputs that the NN can have, so the NN can find the nearest number in this
+list to give your the right output.
+
+=item ERROR_OK
+
+The maximal error of the calculated output.
+
+=item IN_SIZE
+
+The input size (number of nodes in the inpute layer).
+
+=item OUT_SIZE
+
+The output size (number of nodes in the output layer).
+
+=item @HIDDEN_LAYERS
+
+A list of size of hidden layers. By default we have 1 hidden layer, and
+the size is calculated by I<(IN_SIZE + OUT_SIZE)>. So, for a NN of
+2 inputs and 1 output the hidden layer have 3 nodes.
+
+=item %CONF
+
+Conf can be used to define special parameters of the NN:
+
+Default:
+
+ {random_connections=>0 , networktype=>'feedforward' , random_weights=>1 , learning_algorithm=>'backprop' , learning_rate=>0.1 , bias=>1}
+
+=back
+
+Here's a completly example of use:
+
+  my $nn = AI::NNEasy->new(
+  'xor.nne' , ## file to save the NN.
+  [0,1] ,     ## Output types of the NN.
+  0.1 ,       ## Maximal error for output.
+  2 ,         ## Number of inputs.
+  1 ,         ## Number of outputs.
+  [3] ,       ## Hidden layers. (this is setting 1 hidden layer with 3 nodes).
+  {random_connections=>0 , networktype=>'feedforward' , random_weights=>1 , learning_algorithm=>'backprop' , learning_rate=>0.1 , bias=>1} ,
+  ) ;
+
+And a simple example that will create a NN equal of the above:
+
+  my $nn = AI::NNEasy->new('xor.nne' , [0,1] , 0.1 , 2 , 1 ) ;
+
+=head2 load
+
+Load the NN if it was previously saved.
+
+=head2 save
+
+Save the NN to a file using L<Storable>.
+
+=head2 learn (@IN , @OUT , N)
+
+Learn the input.
+
+=over 4
+
+=item @IN
+
+The values of one input.
+
+=item @OUT
+
+The values of the output for the input above.
+
+=item N
+
+Number of times that this input should be learned. Default: 100
+
+Example:
+
+  $nn->learn( [0,1] , [1] , 10 ) ;
+
+=back
+
+=head2 learn_set (@SET , OK_OUTPUTS , LIMIT , VERBOSE)
+
+Learn a set of inputs until get the right error for the outputs.
+
+=over 4
+
+=item @SET
+
+A list of inputs and outputs.
+
+=item OK_OUTPUTS
+
+Minimal number of outputs that should be OK when calculating the erros.
+
+By default I<OK_OUTPUTS> should have the same size of number of different
+inouts in the @SET.
+
+=item LIMIT
+
+Limit of interations when learning. Default: 30000
+
+=item VERBOSE
+
+If TRUE turn verbose method ON when learning.
+
+=back
+
+=head2 get_set_error (@SET , OK_OUTPUTS)
+
+Get the actual error of a set in the NN. If the returned error is bigger than
+I<ERROR_OK> defined on I<new()> you should learn or relearn the set.
+
+=head2 run (@INPUT)
+
+Run a input and return the output calculated by the NN based in what the NN already have learned.
+
+=head2 run_get_winner (@INPUT)
+
+Same of I<run()>, but the output will return the nearest output value based in the
+I<@OUTPUT_TYPES> defined at I<new()>.
+
+For example an input I<[0,1]> learned that have
+the output I<[1]>, actually will return something like 0.98324 as output and
+not 1, since the error never should be 0. So, with I<run_get_winner()>
+we get the output of I<run()>, let's say that is 0.98324, and find what output
+is near of this number, that in this case should be 1. An output [0], will return
+by I<run()> something like 0.078964, and I<run_get_winner()> return 0.
+
+=head1 INLINE C
+
+Some functions of this module have I<Inline> functions writed in C.
+
+I have made a C version only for the functions that are wild called, like:
+
+  AI::NNEasy::NN::tanh
+
+  AI::NNEasy::NN::feedforward::run
+  
+  AI::NNEasy::NN::backprop::hiddenToOutput
+  AI::NNEasy::NN::backprop::hiddenOrInputToHidden
+
+What give use the speed that we need to learn fast the inputs, but at the same time
+be able to create flexible NN.
+
+=head1 Class::HPLOO
+
+I have used L<Class::HPLOO> to write fast the module, specially the XS support.
+
+L<Class::HPLOO> enables this kind of syntax for Perl classes:
+
+  class Foo {
+    
+    sub bar($x , $y) {
+      $this->add($x , $y) ;
+    }
+    
+    sub[C] int add( int x , int y ) {
+      int res = x + y ;
+      return res ;
+    }
+    
+  }
+
+What make possible to write the module in 2 days! ;-P
+
+=head1 Basics of a Neural Network
+
+I<- This is just a simple text for lay pleople,
+to try to make them to understand what is a Neural Network and how it works
+without need to read a lot of books -.>
+
+A NN is based in nodes/neurons and layers, where we have the input layer, the hidden layers and the output layer.
+
+For example, here we have a NN with 2 inputs, 1 hidden layer, and 2 outputs:
+
+         Input  Hidden  Output
+ input1  ---->n1\    /---->n4---> output1
+                 \  /
+                  n3
+                 /  \
+ input2  ---->n2/    \---->n5---> output2
+
+
+Basically, when we have an input, let's say [0,1], it will active I<n2>, that will
+active I<n3> and I<n3> will active I<n4> and I<n5>, but the link between I<n3> and I<n4> has a I<weight>, and
+between I<n3> and I<n5> another I<weight>. The idea is to find the I<weights> between the
+nodes that can give to use an output near the real output. So, if the output of [0,1]
+is [1,1], the nodes I<output1> and I<output2> should give to use a number near 1,
+let's say 0.98654. And if the output for [0,0] is [0], I<output1> and I<output2> should give to use a number near 0,
+let's say 0.078875.
+
+What is hard in a NN is to find this I<weights>. By default L<AI::NNEasy> uses
+I<backprop> as learning algorithm. With I<backprop> it pastes the inputs through
+the Neural Network and ajust the I<weights> using random numbers until we find
+a set of I<weights> that give to use the right output.
+
+The secret of a NN is the number of hidden layers and nodes/neurons for each layer.
+Basically the best way to define the hidden layers is 1 layer of (INPUT_NODES+OUTPUT_NODES).
+So, a layer of 2 input nodes and 1 output node, should have 3 nodes in the hidden layer.
+This definition exists because the number of inputs define the maximal variability of
+the inputs (N**2 for bollean inputs), and the output defines if the variability is reduced by some logic restriction, like
+int the XOR example, where we have 2 inputs and 1 output, so, hidden is 3. And as we can see in the
+logic we have 3 groups of inputs:
+
+  0 0 => 0 # false
+  0 1 => 1 # or
+  1 0 => 1 # or
+  1 1 => 1 # true
+
+Actually this is not the real explanation, but is the easiest way to understand that
+you need to have a number of nodes/neuros in the hidden layer that can give the
+right output for your problem.
+
+Other inportant step of a NN is the learning fase. Where we get a set of inputs
+and paste them through the NN until we have the right output. This process basically
+will adjust the nodes I<weights> until we have a output near the real output that we want.
+
+Other important concept is that the inputs and outputs in the NN should be from 0 to 1.
+So, you can define sets like:
+
+  0 0      => 0
+  0 0.5    => 0.5
+  0.5 0.5  => 1
+  1 0.5    => 0
+  1 1      => 1
+
+But what is really recomended is to always use bollean values for inputs and outputs,
+since the learning fase will be faster.
+
+=head1 SEE ALSO
+
+L<AI::NNFlex>, L<AI::Neural::Simple>, L<Class::HPLOO>, L<Inline>.
+
+=head1 AUTHOR
+
+Graciliano M. P. <gmpassos@cpan.org>
+
+I will appreciate any type of feedback (include your opinions and/or suggestions). ;-P
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=cut
+
